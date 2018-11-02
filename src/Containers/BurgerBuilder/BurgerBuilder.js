57e09cfeef2db566b0fa82ed60cb7c983d3ad126
DaehyunSim <sim.daehyun92@gmail.com>
2018-11-01 21:56:56 -0400

    Added Order Button
    Bug Fixes:
    -Fixed chese to cheese in INGREDIENT_PRICES

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -6,7 +6,7 @@ import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
-    chese: 0.4,
+    cheese: 0.4,
     meat: 1.3,
     bacon: 0.7
 };
@@ -19,7 +19,8 @@ class BurgerBuilder extends Component{
             cheese: 0,
             meat: 0,
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
     };
 
     render () {
@@ -35,9 +36,24 @@ class BurgerBuilder extends Component{
                 <BuildControls 
                     IngredientAdded={this.addIngredientHandler}
                     IngredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}/>
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}/>
+                
             </Auxil>
         );
+    }
+
+    updatePurchaseState = (ingredients) => {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey]
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+
+        this.setState({purchasable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
@@ -50,7 +66,7 @@ class BurgerBuilder extends Component{
         const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
 
         this.setState({totalPrice: newPrice, ingredients:updatedIngredients});
-
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -67,6 +83,8 @@ class BurgerBuilder extends Component{
         const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
 
         this.setState({totalPrice:newPrice, ingredients:updatedIngredients})
+        this.updatePurchaseState(updatedIngredients);
+
     }
 }
 
